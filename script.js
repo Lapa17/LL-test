089e06e89a0dfa4cf82ea9dc5274dfecc6b729ab
Lapa17 <laparevichpavel@gmail.com>
2022-05-12 12:37:50 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+function increment(e) {
+    let value = e.currentTarget.parentNode.childNodes[4].innerHTML
+    e.currentTarget.parentNode.childNodes[4].innerHTML = (Number(value) + 1).toString()
+
+}
+function decrement(e) {
+    let value = e.currentTarget.parentNode.childNodes[4].innerHTML
+    e.currentTarget.parentNode.childNodes[4].innerHTML = (Number(value) - 1).toString()
+}
+
+
+let index = 0
+
+function createCounter (number = 0) {
+    let root = document.getElementById('root')
+    root.innerHTML += "<div class='wrapper'><button onclick='increment(event)' class='plus'>+</button><button onclick='decrement(event)' class='minus'>-</button> <span>Counter:</span><span class='number' ></span></div>"
+    let spans = document.getElementsByClassName('number')
+    spans[index].innerHTML = number.toString();
+    index++
+    return {
+        counter: number,
+        increment: function (){
+            return this.counter += 1
+        },
+        decrement: function (){
+            return this.counter -= 1
+        }
+    }
+}
+
+
+
+const counter_1 = createCounter(15)
+const counter_2 = createCounter(-6)
+const counter_3 = createCounter(22)
+const counter_4 = createCounter()
+
+
+
+
+counter_3.decrement()
+console.log(counter_3.counter)
+counter_1.increment()
+console.log(counter_1.counter)
+
+
